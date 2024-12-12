8064d6f913bf794a0c5943c4512ae0227113f4c6
EnAnsari <rahmat2022a@gmail.com>
2024-12-12 17:18:11 +0330

    fix: fix an error in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 # create data address file
 with open(os.path.join('rcph', 'config', 'data.py'), 'w') as file:
-    file.write(f"DATA_ADDRESS = \'{os.path.join(os.getcwd(), DATA_FOLDER).replace('\\', '/')}\'")
+    data_path = os.path.join(os.getcwd(), DATA_FOLDER).replace('\\', '/')
+    file.write(f"DATA_ADDRESS = \'{data_path}\'")
 
 setup(
     name="rcph",
